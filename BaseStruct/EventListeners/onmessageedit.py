2b5a339217ed9831f7d7a5ea64af2e2b45441b93
EC2 Default User <ec2-user@ip-172-31-19-4.eu-west-1.compute.internal>
2017-10-09 23:32:43 +0000

    Added docstrings to listeners

--- BaseStruct/EventListeners/onmessageedit.py
+++ BaseStruct/EventListeners/onmessageedit.py
@@ -1,4 +1,6 @@
 def listen(client, main):
+    """Listens for when messages are editted in discord, 
+        sends them to the message handler"""
 
     @client.event
     async def on_message_edit(old, message):
--- BaseStruct/EventListeners/onready.py
+++ BaseStruct/EventListeners/onready.py
@@ -1,4 +1,6 @@
 def listen(client, main):
+    """Verifies that the client is connected to discord 
+        using the "connected" flag"""
 
     @client.event
     async def on_ready(): 
